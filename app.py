87f6533d6818200f73824c0cfb49878e0834d529
Samuel Kleymann <110097208+samkley@users.noreply.github.com>
2024-12-05 10:21:18 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import requests
 from flask import Flask, render_template, request, jsonify, send_from_directory
+from tensorflow.keras.models import load_model
 from classify import classify_image
-import os
+
+# Set environment variable to disable GPU (for non-GPU environments)
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 app = Flask(__name__)
 
@@ -11,9 +14,33 @@ UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Dropbox direct download link for the Keras model
+MODEL_URL = 'https://www.dropbox.com/scl/fi/7b01rybaqxoqghuos3i5y/vgg16_model.keras?rlkey=7c8l93edlhpzqwfl078980bcn&st=o7yo2azk&dl=1'
+
+# Path to save the model
+MODEL_PATH = 'vgg16_model.keras'
+
 # Helper function to check allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Function to download the model if it doesn't exist
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print(f"Downloading model from {MODEL_URL}...")
+        response = requests.get(MODEL_URL)
+        
+        # Check if the download was successful
+        if response.status_code == 200:
+            with open(MODEL_PATH, 'wb') as f:
+                f.write(response.content)
+            print("Model downloaded successfully.")
+        else:
+            print(f"Failed to download model. Status code: {response.status_code}")
+
+# Load the model when the application starts
+download_model()
+model = load_model(MODEL_PATH)
 
 @app.route('/')
 def home():
@@ -35,7 +62,7 @@ def upload_image():
         file.save(filename)
 
         try:
-            # Classify the image
+            # Classify the image using the downloaded model
             class_name, probability = classify_image(filename)
             if class_name is None:
                 return jsonify({"error": "Error during image classification"}), 500
@@ -73,4 +100,3 @@ def feedback():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
-
