2837f8053a6afa736ef59faba1384b387a4ddf12
Samuel Kleymann <110097208+samkley@users.noreply.github.com>
2024-12-05 10:21:26 +1300

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.models import save_model
 
 # Load a pre-trained VGG16 model with ImageNet weights
 model = VGG16(weights='imagenet')
